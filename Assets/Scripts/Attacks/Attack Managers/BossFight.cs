63843471492d9b5dbc429a6c4d2ca702c5646663
BluCherry <cgroves317@gmail.com>
2026-05-11 14:34:03 -0500

    boss

---
+++ Assets/Scripts/Attacks/Attack Managers/BossFight.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class BossFight : MonoBehaviour
+{
+    private GameObject target;
+    private ShootAllDirection shoot;
+    private TimedSpawner spawn;
+
+    [SerializeField]
+    float agroDistance = 10f;
+
+    bool isAgro = false;
+
+    private void Start()
+    {
+        target = GameObject.Find("Player");
+        shoot = GetComponent<ShootAllDirection>();
+        spawn = GetComponent<TimedSpawner>();
+        shoot.enabled = false;
+        spawn.enabled = false;
+    }
+
+    void Update()
+    {
+        if (!isAgro && (target.transform.position - transform.position).magnitude <= agroDistance)
+        {
+            isAgro = true;
+            ActivateFight();
+        }
+    }
+
+    void ActivateFight()
+    {
+        shoot.enabled = true;
+        spawn.enabled = true;
+    }
+}
